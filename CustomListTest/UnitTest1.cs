68358cf416724ca2d659d432655b4409bc9d5fd0
wchandler2018 <36997950+wchandler2018@users.noreply.github.com>
2020-01-24 16:33:02 -0600

    created VerifyCountAfterAddSecondValue, VerifyCountAfterAddMultipleValue and VerifyTotalIndexCountValue

--- CustomListTest/UnitTest1.cs
+++ CustomListTest/UnitTest1.cs
@@ -15,25 +15,84 @@ namespace CustomListTest
             int value = 10;
 
             //Act
-            willsList.add(value);
+            willsList.Add(value);
             //Assert
-            Assert.AreEqual(value, willsList[0]);          
+            Assert.AreEqual(1, willsList.Count);          
         }
 
         [TestMethod]
 
-        public void VerifyCountAfterAdd()
+        public void VerifyCountAfterAddValue()
         {
             //Arrange
             WilliamsCustomList<int> willsList = new WilliamsCustomList<int>();
-            int value = 5;
-            int expectResult = 1;
+            int value = 6;
+            int expectResult = 6;
             //Act
-            willsList.add(value);
+            willsList.Add(value);
+            
             //Assert
-            Assert.AreEqual(value, willsList[]);
-
-
+            Assert.AreEqual(expectResult, willsList[0]);
         }
+
+        public void VerifyCountAfterAddSecondValue()
+        {
+            //Arrange
+            WilliamsCustomList<int> willsList = new WilliamsCustomList<int>();
+            int value = 6;
+            int secondValue = 12;
+            int expectResult = 12;
+            //Act
+            willsList.Add(value);
+            willsList.Add(secondValue);
+
+            //Assert
+            Assert.AreEqual(expectResult, willsList[1]);
+        }
+
+        public void VerifyCountAfterAddMultipleValue()
+        {
+            //Arrange
+            WilliamsCustomList<int> willsList = new WilliamsCustomList<int>();
+            int value = 6;
+            int secondValue = 12;
+            int thirdValue = 18;
+            int fourthValue = 24;
+            int fifthValue = 30;
+
+            int expectResult = 30;
+            //Act
+            willsList.Add(value);
+            willsList.Add(secondValue);
+            willsList.Add(thirdValue);
+            willsList.Add(fourthValue);
+            willsList.Add(fifthValue);
+
+            //Assert
+            Assert.AreEqual(expectResult, willsList[4]);
+        }
+
+        public void VerifyTotalIndexCountValue()
+        {
+            //Arrange
+            WilliamsCustomList<int> willsList = new WilliamsCustomList<int>();
+            int value = 6;
+            int secondValue = 12;
+            int thirdValue = 18;
+            int fourthValue = 24;
+            int fifthValue = 30;
+
+            int expectResult = 5;
+            //Act
+            willsList.Add(value);
+            willsList.Add(secondValue);
+            willsList.Add(thirdValue);
+            willsList.Add(fourthValue);
+            willsList.Add(fifthValue);
+
+            //Assert
+            Assert.AreEqual(expectResult, willsList.Count);
+        }
+
     }
 }
